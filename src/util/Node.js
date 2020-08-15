b60cb48693b3d0848d57e39c446cca9e2fa649e6
James Langley <jwblangley@gmail.com>
2020-08-15 16:39:34 +0100

    Implement createGraphFromGrid

--- src/util/Node.js
+++ src/util/Node.js
@@ -20,6 +20,29 @@ export class Node {
     addNeighbour(neighbour) {
         this._neighbours.push(neighbour)
     }
+
+    static graphFromGrid(grid) {
+        const nodes = grid.map((row, i) => row.map((item, j) => new Node([i, j], item)))
+        for (let i = 0; i < nodes.length; i++) {
+            for (let j = 0; j < nodes[0].length; j++) {
+
+                const node = nodes[i][j]
+
+                for (let k = -1; k <= 1; k++) {
+                    for (let l = -1; l <= 1; l++) {
+                        if (k == 0 && l == 0) {
+                            continue; // Don't add self
+                        }
+                        if (i + k >= 0 && i + k < nodes.length
+                            && j + l >= 0 && j + l < nodes[0].length) {
+                            node.addNeighbour(nodes[i + k][j + l])
+                        }
+                    }
+                }
+            }
+        }
+        return nodes
+    }
 }
 
 export default Node
