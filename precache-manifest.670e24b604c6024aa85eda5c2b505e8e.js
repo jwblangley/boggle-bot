96691179f2c56c1c6dcb3bd38ad0f60c54d457b4
James Langley <jwblangley@gmail.com>
2020-09-10 13:11:23 +0100

    Updates

---
+++ precache-manifest.670e24b604c6024aa85eda5c2b505e8e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ec696b3670a48b3e4a89b94d8545132",
+    "url": "/boggle-bot/778bc659fa9357d32747.worker.js"
+  },
+  {
+    "revision": "b0194e60f133d4c4f0f66c49deb8886b",
+    "url": "/boggle-bot/index.html"
+  },
+  {
+    "revision": "d96d827b75506c16faaa",
+    "url": "/boggle-bot/static/css/main.27f58697.chunk.css"
+  },
+  {
+    "revision": "dfab08f0ca547c0524f8",
+    "url": "/boggle-bot/static/js/2.d3e465b7.chunk.js"
+  },
+  {
+    "revision": "5d5bed4b9798bc2a77341347871396b6",
+    "url": "/boggle-bot/static/js/2.d3e465b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d96d827b75506c16faaa",
+    "url": "/boggle-bot/static/js/main.ec1a0eee.chunk.js"
+  },
+  {
+    "revision": "29dbb59e7c98ac77373f",
+    "url": "/boggle-bot/static/js/runtime-main.5e8f4784.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.26efb5d8ffb5811fe34ecc553bd61a76.js"
+  "/boggle-bot/precache-manifest.670e24b604c6024aa85eda5c2b505e8e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/boggle-bot/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
